767091a20d8efd0323e445ef570b0b8b6dc296c2
Mouhamadou Gueye <mogueye87@gmail.com>
2020-01-17 01:42:50 +0100

    Setting up update_docker file

--- project4-operationalize-machine-learning-microservice/upload_docker.sh
+++ project4-operationalize-machine-learning-microservice/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="mogueye87/ml-microservice-prediction:v0.1"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker image tag ml-microservice-prediction $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
