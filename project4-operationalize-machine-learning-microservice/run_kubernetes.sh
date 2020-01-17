61df2cc612e4fa3b958259b15b0d2a4ffe417fe0
Mouhamadou Gueye <mogueye87@gmail.com>
2020-01-17 01:40:04 +0100

    Setting up the run_kubernetes files

--- project4-operationalize-machine-learning-microservice/run_kubernetes.sh
+++ project4-operationalize-machine-learning-microservice/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=mogueye87/ml-microservice-prediction:v0.1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservice-prediction --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $(kubectl get pods -o=name | grep ml-microservice-prediction | sed "s/^.\{4\}//") 8000:80
